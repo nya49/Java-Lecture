b1878e9b82de30e87b873f571ba8de99ac9e5cff
Yeonah <nya49@naver.com>
2019-03-28 09:00:09 +0900

    OpenChallenge

--- OpenChallenge/src/JavaCodingT_0327/ex01.java
+++ OpenChallenge/src/JavaCodingT_0327/ex01.java
@@ -5,7 +5,8 @@ public class ex01 {
 	public static void main(String[] args) {
 		
 		int total = 0;
-
+		
+		System.out.println("10~1000까지 각 숫자를 분해하여 곱하기한 값의 전체 합을 구하시오.");
         for(int i=10; i<=1000; i++){
 
             Integer num = i; 
--- OpenChallenge/src/JavaCodingT_0327/ex02.java
+++ OpenChallenge/src/JavaCodingT_0327/ex02.java
@@ -5,15 +5,31 @@ import java.util.Scanner;
 public class ex02 {
 
 	public static void main(String[] args) {
+		
 		Scanner scan = new Scanner(System.in);
 		
-		String dashInsert = scan.nextLine();
-		String[] nums = dashInsert.split("");
+		String input =scan.nextLine();
 		
-		for(String num : nums) {
-		System.out.print(num);
-		}
 		
-	}
+        StringBuilder number = new StringBuilder(input);
+        String[] num = input.split("");
 
+
+        for(int i=0, j=0; i<num.length-1 ; i++,j++) {
+
+            int a = Integer.parseInt(String.valueOf(num[i]));
+            int b = Integer.parseInt(String.valueOf(num[i+1]));
+            if(a%2==0 && b%2==0) {
+                number.insert(j+1,"*");
+                j++;
+            }else if ((a!=0&&b!=0) && (a%2!=0 && b%2!=0)) {
+                number.insert(j+1, "-");
+                j++;
+            }
+
+        }
+
+        System.out.print(number);
+
+        }
 }
