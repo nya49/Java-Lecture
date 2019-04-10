09c3faa9187f58e7753e00ff929649e09bda050b
Yeonah <nya49@naver.com>
2019-04-10 16:36:47 +0900

    OpenChallenge

--- OpenChallenge/src/JavaCodingT_0410/ex01/PersonExample.java
+++ OpenChallenge/src/JavaCodingT_0410/ex01/PersonExample.java
@@ -10,23 +10,23 @@ public class PersonExample {
 				+ "최승혁,이성연,박영서,박민호,전경헌,송정환,김재성,이유덕,"
 				+ "이재영,전경헌" );
 		String name[] = names.split(",");
-		int kimC = 0;
-		int leeC = 0;
+		int kim = 0;
+		int lee = 0;
 
 		// 1번
 		for (int i = 0; i < name.length; i++) {
 			if (String.valueOf(name[i].charAt(0)).equals("김")) {
-				kimC++;
+				kim++;
 			}
 		}
-		System.out.println("중복을 포함한 김씨 수 : " + kimC + "명");
+		System.out.println("중복을 포함한 김씨 수 : " + kim + "명");
 
 		for (int i = 0; i < name.length; i++) {
 			if (String.valueOf(name[i].charAt(0)).equals("이")) {
-				leeC++;
+				lee++;
 			}
 		}
-		System.out.println("중복을 포함한 이씨 수 : " + leeC + "명");
+		System.out.println("중복을 포함한 이씨 수 : " + lee + "명");
 
 		// 2번
 		int jaeyoung = 0;
@@ -44,18 +44,16 @@ public class PersonExample {
 				noOverlap += (name[i] + " ");
 			}
 		}
-		System.out.print("중복제거 이름 : ");
-		System.out.println(noOverlap);
+		System.out.println("중복제거 후 이름 : " + noOverlap );
 
 		// 4번
 		String noOverlapnames[] = noOverlap.split(" ");
 		Arrays.sort(noOverlapnames);
-		System.out.print("중복제거 이름 : ");
+		System.out.print("중복제거 이름 오름차순: ");
 		for (int i = 0; i < noOverlapnames.length; i++) {
 			System.out.print(noOverlapnames[i] + " ");
 		}
-
-
+		
 	}
 
 }
--- OpenChallenge/src/JavaCodingT_0410/ex02/PersonExample.java
+++ OpenChallenge/src/JavaCodingT_0410/ex02/PersonExample.java
@@ -14,46 +14,51 @@ public class PersonExample {
 		
 		String[] arr = names.split(",");
 		
-		ArrayList<String> list = new ArrayList<String>();
+		ArrayList<String> name = new ArrayList<String>();
+		for(int i=0; i<arr.length; i++) {
+			name.add(arr[i]);
+		}
 		
+		int kim = 0;
+		int lee = 0;
+		
+		// 1번
+		for (int i = 0; i < name.size(); i++) {
+			if (name.get(i).contains("김")) {
+				kim++;
+			}
+		}
+		
+		System.out.println("중복을 포함한 김씨 수 : " + kim + "명");
+
+		for (int i = 0; i < name.size(); i++) {
+			if (name.get(i).contains("이")) {
+				lee++;
+			}
+		}
+		System.out.println("중복을 포함한 이씨 수 : " + lee + "명");
+
+		// 2번
+		int jaeyoung = 0;
+		for (int i = 0; i < name.size(); i++) {
+			if (name.get(i).contains("이재영")) {
+				jaeyoung++;
+			}
+		}
+		System.out.println("이재영이름 중복 횟수 : " + jaeyoung + "번");
+		
+		ArrayList<String> list = new ArrayList<String>();
 		for(int i=0; i< arr.length; i++) {
 			if(!list.contains(arr[i])) {
 				list.add(arr[i]);
 			}
 		}
 		
-		int kimC = 0;
-		int leeC = 0;
-		
-		// 1번
-		for (int i = 0; i < list.size(); i++) {
-			if (list.get(i).contains("김")) {
-				kimC++;
-			}
-		}
-		System.out.println("중복을 포함한 김씨 수 : " + kimC + "명");
-
-		for (int i = 0; i < list.size(); i++) {
-			if (list.get(i).contains("이")) {
-				leeC++;
-			}
-		}
-		System.out.println("중복을 포함한 이씨 수 : " + leeC + "명");
-
-		// 2번
-		int jaeyoung = 0;
-		for (int i = 0; i < list.size(); i++) {
-			if (list.get(i).contains("이재영")) {
-				jaeyoung++;
-			}
-		}
-		System.out.println("이재영이름 중복 횟수 : " + jaeyoung + "번");
-		
 		// 3번
-		System.out.println("중복제거 이름 : " + list);
+		System.out.println("중복제거 후 이름 : " + list);
 		
 		Collections.sort(list);
-		System.out.println("중복제거 이름 : " + list);
+		System.out.println("중복제거 이름 오름차순: " + list);
 
 	}
 	
